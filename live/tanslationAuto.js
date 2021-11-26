44344109ad1442a6a2d42d6332f4b0fef3c89e4b
JINxo-geek <qq527382125@gmail.com>
2021-11-26 23:14:22 +0800

    leidian

--- live/tanslationAuto.js
+++ live/tanslationAuto.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @include      *.*.*
+// @include      *
 // @include      *.quora.com/*
 // @include      *.twitter.com/*
 // @grant        none
